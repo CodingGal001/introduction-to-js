0be7698d90734c50362f82c57871433446a419af
CodingGal001 <adeyemoayomide9899@gmail.com>
2022-12-07 15:13:32 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+function Show_Detail() {
+
+    var username = document.getElementById('username').value;
+    var age = document.getElementById('age').value;
+
+    alert('Welcome ' + username + ',' + ' You are ' +  age + ' years old');
+
+}
